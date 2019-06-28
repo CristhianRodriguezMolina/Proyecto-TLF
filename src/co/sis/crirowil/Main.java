25e15de1e4ed34845e20ba6c19b7a18cf95e61b4
Wilmar Valencia <wilmar2545@gmail.com>
2019-06-27 19:06:34 -0500

    synch

--- src/co/sis/crirowil/Main.java
+++ src/co/sis/crirowil/Main.java
@@ -1,23 +1,26 @@
 package co.sis.crirowil;
 
+import co.sis.crirowil.persistencia.AnalizadorLexico;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-		/**
-		 * 
-		 * @param args
-		 */
-		public static void main(String[] args) {
-			launch(args);
-		}
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String msj = "4636346sfhaoihaof3432423asd35af34";
+		AnalizadorLexico analizador = new AnalizadorLexico(msj);
+		analizador.analizar();
+		System.out.println(analizador.getListaTokens());
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
 		
-		/**
-		 * 
-		 */
-		public void start(Stage primaryStage) {
-			
-		}
-	
+	}
+
 }
