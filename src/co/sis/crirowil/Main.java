32710f7d4c2a676b4b4652fc0320de3b04ee7d3c
Wilmar Valencia <wilmar2545@gmail.com>
2019-06-28 17:11:57 -0500

    caso real de prueba

--- src/co/sis/crirowil/Main.java
+++ src/co/sis/crirowil/Main.java
@@ -10,13 +10,37 @@ public class Main extends Application {
 	 * @param args
 	 */
 	public static void main(String[] args) {
- 		String codigoFuente = ".GAWLVK;;cicloAWKentero"+
- 		"RG´{{}787/58}*======***sdar"+
- 		"ealsd3***45^=24531//212@5.fgdg=f." +
- 		"345\n.fdt@rer++=`{{{[[[^tert,sdg."+
-		"hx245hxAB453yhx245.7126574adasenteroadgasd."+
- 		"enterohxA324.hxACFE342AFhx."+
-		"hx2hx34hxl?sdj.hxaoer.hx4phx";
+ 		String codigoFuente = "importar mundo@\r\n" + 
+ 				"\r\n" + 
+ 				"$$Comentario de prueba$$\r\n" + 
+ 				"metodo main(args, resp)\r\n" + 
+ 				"{\r\n" + 
+ 				"	entero numero1 = 345@\r\n" + 
+ 				"	real numero2 = 5.63@\r\n" + 
+ 				"	real numero3 = .45@\r\n" + 
+ 				"	real numero4 = 456.@\r\n" + 
+ 				"	cadena = \"hola mundo\"@\r\n" + 
+ 				"	ciclo(entero i = 0; i < 50; i+=1)\r\n" + 
+ 				"	{\r\n" + 
+ 				"		cadena temp = \"\"@\r\n" + 
+ 				"		entero res = numero2*numero3@\r\n" + 
+ 				"		entero res1 *= numero1^numero4@\r\n" + 
+ 				"		string res2 = \"hola\" + \"mundo\" + \"\"@\r\n" + 
+ 				"		sisas(numero1 >= numero 3)\r\n" + 
+ 				"		{\r\n" + 
+ 				"			cadena hexa = hx123456789ABCDEF + \"\"@\r\n" + 
+ 				"		}\r\n" + 
+ 				"		nonasis(numero3 == numero4 yy 5<4 oo n4<3) \r\n" + 
+ 				"		{\r\n" + 
+ 				"\r\n" + 
+ 				"		}\r\n" + 
+ 				"		nonas\r\n" + 
+ 				"		{\r\n" + 
+ 				"\r\n" + 
+ 				"		}\r\n" + 
+ 				"	}\r\n" + 
+ 				"	devolver nada@\r\n" + 
+ 				"}";
 		AnalizadorLexico al = new AnalizadorLexico(codigoFuente);
 		al.analizar();
 		System.out.println(al.getListaTokens());
